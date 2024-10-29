9b647061896095a050956a4e6856eac87cbb1146
Sergi-SL <sergi@sharplaunch.com>
2024-10-29 23:56:30 +0100

    adding sqlite

--- websocket.js
+++ websocket.js
@@ -11,6 +11,18 @@ server.listen(3000, function () {
     console.log('Server listening on port 3000');
 });
 
+// On Signal Interrupt
+process.on('SIGINT', () => {
+    //Close all websocket connections
+    wss.clients.forEach((client) => {
+        client.close();
+    });
+    // Close the server and the db
+    server.close(() => {
+        shutDownDb();
+    })
+});
+
 // Begin Websocket
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server });
@@ -27,6 +39,11 @@ wss.on('connection', function (ws) {
         ws.send('Welcome!');
     }
 
+    db.run(`
+        INSERT INTO visitors VALUES (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close() {
         wss.broadcast(`Current Visitors: ${numClients}`);
         console.log('Client Disconnected');
@@ -38,3 +55,34 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 };
+
+// Begin Database
+const sqlite = require('sqlite3');
+
+// This would save to a db file:
+// const db = new sqlite.Database('./database.db');
+
+// This would create a new in-memory db:
+const db = new sqlite.Database(':memory:');
+
+// Initialize the db before anything so that we cnanot query or write tot ables that do not exist
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+function shutDownDb() {
+    getCounts();
+    console.log('Shutting down db');
+    db.close();
+}
